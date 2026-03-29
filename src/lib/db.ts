8e2692d22b5d90f145be82e39be4373d886e8339
jshmslf <jshmslf@gmail.com>
2026-03-29 23:13:43 +0800

    fixed db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
 
 function createClient() {
   const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
   return new PrismaClient({ adapter, log: ['error'] })
 }
 
-export const db = globalForPrisma.prisma ?? createClient()
+export const db = globalThis.prisma ?? createClient()
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
